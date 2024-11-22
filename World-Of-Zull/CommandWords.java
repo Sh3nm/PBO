e2d895fdada880f8a2ea2498ee9f2cb29cb0c237
Shenmue <162028242+Sh3nm@users.noreply.github.com>
2024-11-23 00:17:33 +0700

    Add files via upload

---
+++ World-Of-Zull/CommandWords.java
@@ -0,0 +1,21 @@
+public class CommandWords
+{
+    private static final String[] validCommands = {
+        "go", "quit", "help"
+    };
+
+    public CommandWords()
+    {
+        // nothing to do at the moment...
+    }
+
+    public boolean isCommand(String aString)
+    {
+        for(int i = 0; i < validCommands.length; i++) {
+            if(validCommands[i].equals(aString))
+                return true;
+        }
+        // if we get here, the string was not found in the commands
+        return false;
+    }
+}
